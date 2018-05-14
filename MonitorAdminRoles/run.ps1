344ed764ade2270c10a50b5024f1ac299967dbee
John Barrar <jbarrar@hotmail.com>
2018-05-14 04:52:35 -0400

    testing

--- MonitorAdminRoles/run.ps1
+++ MonitorAdminRoles/run.ps1
@@ -15,10 +15,7 @@ $username = "o365tasks@prxcsp.com"
 # MSI Variables via Function Application Settings Variables
 # Endpoint and Password
 $endpoint = $env:MSI_ENDPOINT
-$endpoint
 $secret = $env:MSI_SECRET
-$secret
-
 # Vault URI to get AuthN Token
 $vaultTokenURI = 'https://vault.azure.net&api-version=2017-09-01'
 # Our Key Vault Credential that we want to retreive URI
@@ -26,26 +23,29 @@ $vaultTokenURI = 'https://vault.azure.net&api-version=2017-09-01'
 $vaultSecretURI = 'https://celeritasvault.vault.azure.net/secrets/AdminPassSecureString/bb11e2f53cc14ac984e4649cf0bb6b71/?api-version=2015-06-01'
 # Create AuthN Header with our Function App Secret
 $header = @{'Secret' = $secret}
-
 # Get Key Vault AuthN Token
 $authenticationResult = Invoke-RestMethod -Method Get -Headers $header -Uri ($endpoint +'?resource=' +$vaultTokenURI)
-$authenticationResult
 # Use Key Vault AuthN Token to create Request Header
 $requestHeader = @{ Authorization = "Bearer $($authenticationResult.access_token)" }
-
 # Call the Vault and Retrieve Creds
 $creds = Invoke-RestMethod -Method GET -Uri $vaultSecretURI -ContentType 'application/json' -Headers $requestHeader
-
 write-output "Credential ID: " $($creds.id)
 write-output "Credential Value: " $($creds.value) 
 
-#$pw = Get-AzureKeyVaultSecret -VaultName $keyVaultName -Name "AdminPassSecureString"
+$pw = $creds.value
+
+$vaultSecretURI = 'https://celeritasvault.vault.azure.net/secrets/AdminPassSecureString/bb11e2f53cc14ac984e4649cf0bb6b71/?api-version=2015-06-01'
+$creds = Invoke-RestMethod -Method GET -Uri $vaultSecretURI -ContentType 'application/json' -Headers $requestHeader
+write-output "Credential ID: " $($creds.id)
+write-output "Credential Value: " $($creds.value) 
+
+$key = $creds.value
 
 # Build Credentials
 #$keypath = "D:\home\site\wwwroot\$FunctionName\bin\keys\PassEncryptKey.key"
-#$secpassword = $pw | ConvertTo-SecureString -Key (Get-Content $keypath)
-#$credential = New-Object System.Management.Automation.PSCredential ($username, $secpassword)
-
+$secpassword = $pw | ConvertTo-SecureString -Key $key
+$credential = New-Object System.Management.Automation.PSCredential ($username, $secpassword)
+$credential.Password
 # Connect to MSOnline
 
 #Connect-MsolService -Credential $credential
